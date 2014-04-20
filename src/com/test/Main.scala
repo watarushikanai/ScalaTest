7b56eb9a98ea15a6c94cf0e7554c9d6d32241589
Wataru Shikanai <tarosan1192@gmail.com>
2014-04-20 23:16:06 +0900

    初回コミット

--- src/com/test/Main.scala
+++ src/com/test/Main.scala
@@ -7,7 +7,7 @@ object Main {
     // 基本コンストラクタの生成
     val BasicConstructor = new classObject(0, "BasicConstructor.")
     // 補助コンストラクタの生成1
-    val SuppotConstructor1 = new classObject(1)
+    val SuppotConstructor3 = new classObject(1)
     // 補助コンストラクタの生成2
     val SuppotConstructor2 = new classObject("SuppotConstructor2.")
   }
