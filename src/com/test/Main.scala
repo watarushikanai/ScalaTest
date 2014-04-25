cde6737938e54196c985820a1ed18137ef8733d8
Wataru Shikanai <tarosan1192@gmail.com>
2014-04-26 01:05:46 +0900

    ブログ登録

--- src/com/test/Main.scala
+++ src/com/test/Main.scala
@@ -4,11 +4,25 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    // 基本コンストラクタの生成
-    val BasicConstructor = new classObject(0, "BasicConstructor.")
-    // 補助コンストラクタの生成1
-    val SuppotConstructor3 = new classObject(1)
-    // 補助コンストラクタの生成2
-    val SuppotConstructor2 = new classObject("SuppotConstructor2.")
+    // 関数の定義は変数へ代入可能
+    val fuction = (str: String) => {
+      println(str)
+    }
+
+    // 上記とは動作が異なる関数を定義
+    val fuction2 = (str: String) => {
+      println("It's a Scala" + str)
+    }
+
+    // メソッドの引数へ関数を設定
+    execute(fuction)
+    execute(fuction2)
+
   }
+
+  // メソッドの引数に関数を定義する場合
+  def execute(function: (String) => Unit) {
+    function("World")
+  }
+
 }
