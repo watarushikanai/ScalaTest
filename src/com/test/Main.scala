0fbcc326a90eb91b32b7ca92adcead31bc33388e
Wataru Shikanai <tarosan1192@gmail.com>
2014-04-29 18:55:11 +0900

    getterとsetterの処理結果について

--- src/com/test/Main.scala
+++ src/com/test/Main.scala
@@ -4,25 +4,22 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    // 関数の定義は変数へ代入可能
-    val fuction = (str: String) => {
-      println(str)
-    }
+    // インスタンスの生成
+    val objects = new classObject(10, "first")
 
-    // 上記とは動作が異なる関数を定義
-    val fuction2 = (str: String) => {
-      println("It's a Scala" + str)
-    }
+    // 初期値の出力
+    println("number:" + objects.number)
+    println("str:" + objects.str)
 
-    // メソッドの引数へ関数を設定
-    execute(fuction)
-    execute(fuction2)
+    // numberはvalで定義しているため、setterを呼び出すとコンパイルエラー
+    //　objects.number = 11
 
-  }
+    // varで定義したフィールドへ直接設定しているように見えるが実際は、setterを呼び出している
+    objects.str = "second"
 
-  // メソッドの引数に関数を定義する場合
-  def execute(function: (String) => Unit) {
-    function("World")
+    // フィールドの変更後
+    println("str:" + objects.str)
+
   }
 
 }
--- src/com/test/classObject.scala
+++ src/com/test/classObject.scala
@@ -1,19 +1,7 @@
 package com.test
 
-// 引数ありのコンストラクタの場合はクラス定義に記述する
-class classObject(number: Int, str: String) {
-  // 基本コンストラクタで行う初期処理はクラス定義直後に記述する。
-  println("No." + number + " it's a " + str)
-
-  // 1番目の補助コンストラクタの定義
-  def this(number: Int) {
-    // 必ず別に定義されたコンストラクタを呼び出すこと。(ここは基本コンストラクタのみ指定可能)
-    this(number, "SuppotConstructor1.")
-  }
-
-  // 2番目の補助コンストラクタの定義
-  def this(str: String) {
-    // 必ず別に定義されたコンストラクタを呼び出すこと。(ここは基本コンストラクタまたは1番目の補助コンストラクタを指定可能)
-    this(2, str)
-  }
+// valとvarを定義するとフィールドが自動で定義される
+// valの場合getterが自動で定義される
+// varの場合getterとsetterが自動で定義される(「str_」という名前のsetter)
+class classObject(val number: Int, var str: String) {
 }
